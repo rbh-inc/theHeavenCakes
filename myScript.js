d48b175cab8c3e911bb17ec529bb6374aff69188
Rabeeh T A <rabeehchemnad@gmail.com>
2023-07-03 13:42:17 +0530

    reviews page mvp

--- myScript.js
+++ myScript.js
@@ -1,11 +1,30 @@
 const form = document.getElementById('review-submit-form');
 const loadingSpinner = document.getElementById('loading-spinner');
 
+// Get all the radio buttons
+const ratingRadios = document.querySelectorAll('input[name="rating"]');
+var selectedStar = 0;
+
+// Add event listener to each radio button
+ratingRadios.forEach((radio) => {
+  radio.addEventListener('change', () => {
+    // Get the value of the selected radio button
+    const selectedValue = document.querySelector(
+      'input[name="rating"]:checked'
+    ).value;
+
+    // selected star to a local variable
+    selectedStar = selectedValue;
+  });
+});
+
 form.addEventListener('submit', function (e) {
   e.preventDefault();
   loadingSpinner.style.display = 'block'; // toggle spinner on
-  const data = new FormData(form);
+  var data = new FormData(form);
+  data.append('Stars', selectedStar);
   const action = e.target.action;
+  console.log(data);
   fetch(action, {
     method: 'POST',
     body: data,
